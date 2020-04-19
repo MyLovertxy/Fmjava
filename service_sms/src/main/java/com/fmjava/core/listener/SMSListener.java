ec31529d0e5a71047dfa56b567de3c84fbf4a0e1
wuyong7 <wuyong7@asiainfo.com>
2020-04-19 22:37:54 +0800

    【短信接收方项目搭建】

---
+++ service_sms/src/main/java/com/fmjava/core/listener/SMSListener.java
@@ -0,0 +1,11 @@
+package com.fmjava.core.listener;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class SMSListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+
+    }
+}
